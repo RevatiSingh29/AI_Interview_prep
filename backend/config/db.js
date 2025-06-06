3278a2c725daf85a1e6819b6e1ccfe04542e10a7
RevatiSingh29 <143447751+RevatiSingh29@users.noreply.github.com>
2025-06-06 18:14:47 +0530

    Initial Commit

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,7 @@
 const mongoose =require ("mongoose");
 const connectDB= async() => {
   try{
-    await mongoose.connect("mongodb+srv://test:radhe108@interviewprep.yzk2cvx.mongodb.net/?retryWrites=true&w=majority&appName=InterviewPrep",{});
+    await mongoose.connect("mongodb+srv://test:<yourpassword>@interviewprep.yzk2cvx.mongodb.net/?retryWrites=true&w=majority&appName=InterviewPrep",{});
     console.log("DB connected");
   }
   catch(err){
